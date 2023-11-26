d9277665ecb8b56b7eee081ec0e4dd97b929a9ea
Fatih Inan <mfatihinan@gmail.com>
2023-11-26 18:11:56 -0500

    Created simple page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+import Header from './header/Header'
 
 function App() {
-  return <div className='App'>App</div>
+  return (
+    <BrowserRouter>
+      <div className='app'>
+        <Header />
+        <div className='content'>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <div className='home'>
+                  <h2>Welcome to ChatSimple</h2>
+                  <p>The simplest way to chat with your customers</p>
+                  <nav className='buttons'>
+                    <Link to='/login' className='button'>
+                      Log in
+                    </Link>
+                    <Link to='/register' className='button'>
+                      Sign up
+                    </Link>
+                  </nav>
+                </div>
+              }
+            />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/header/Header.tsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1>Quick Chat</h1>
+      <p>The quickest way to chat with your customers</p>
+    </div>
+  )
+}
+
+export default Header
